2ace40d29a112510b1844b5f1527470f5e7a2d75
Liam Zuiderhoek <zuiderhoekl@gmail.com>
2025-12-27 00:36:21 +0100

    Some buttons and styling

--- frontend/src/components/NavBar.vue
+++ frontend/src/components/NavBar.vue
@@ -1,4 +1,26 @@
 <template>
-  <span><a href="/about">About</a></span>
-  <span><a href="/login">Log In</a></span>
+  <div>
+    <span><a href="/about">About</a></span>
+    <span><a href="/domotics">Domotics</a></span>
+    <span class="right"><a href="/login">Log In</a></span>
+  </div>
 </template>
+
+<style scoped>
+div {
+  position: absolute;
+  top: 0px;
+  left: 0px;
+  width: 100%;
+  text-align: left;
+}
+span {
+  display: inline-block;
+  width: 100px;
+  border: solid;
+}
+.right {
+  position: absolute;
+  right: 0px;
+}
+</style>
